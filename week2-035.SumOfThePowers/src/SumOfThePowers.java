6faca8431130ed5f7062fcb567051fed95b9c1a2
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-12-23 15:57:00 +0100

    Starting week 2

--- week2-035.SumOfThePowers/src/SumOfThePowers.java
+++ week2-035.SumOfThePowers/src/SumOfThePowers.java
@@ -14,7 +14,7 @@ public class SumOfThePowers {
 
         // Sum 2 to the power of n and all the powers before that up to 0.
         // Ex. Input  = 3; Output is 2^0 + 2^1 + 2^2 2^3;
-        // 1 + 2 + 4 + 8 = 15;
+        // 1 + 2 + 4 + 8 = 15
         while(i <= number) {
             total += Math.pow(2, i);
             i++;
