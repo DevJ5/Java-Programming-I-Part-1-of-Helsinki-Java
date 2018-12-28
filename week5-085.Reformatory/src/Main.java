7e356fbc14acf2a74c381129fc5e188829dfa180
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-12-28 16:24:34 +0100

    Half week 5 done

--- week5-085.Reformatory/src/Main.java
+++ week5-085.Reformatory/src/Main.java
@@ -2,6 +2,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write here test code 
+    Person brian = new Person("brian", 20, 160, 70);
+
+
     }
 }
--- week5-085.Reformatory/src/Reformatory.java
+++ week5-085.Reformatory/src/Reformatory.java
@@ -1,8 +1,20 @@
 public class Reformatory {
 
+    private int countMeasurements = 0;
+
     public int weight(Person person) {
         // return the weight of the person
-        return -1;
+        this.countMeasurements++;
+        return person.getWeight();
+    }
+
+    public void feed(Person person) {
+        int currentWeight =  person.getWeight();
+        person.setWeight(currentWeight + 1);
+    }
+
+    public int totalWeightsMeasured(){
+        return this.countMeasurements;
     }
 
 }
