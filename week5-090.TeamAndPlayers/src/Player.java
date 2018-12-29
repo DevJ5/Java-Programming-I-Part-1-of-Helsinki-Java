276892d083f917928c57a44c04bce27eedc46a7f
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-12-29 12:25:18 +0100

    Week4 close

---
+++ week5-090.TeamAndPlayers/src/Player.java
@@ -0,0 +1,28 @@
+public class Player {
+    private String name;
+    private int amountOfGoals;
+
+    public Player(String name, int amountOfGoals) {
+        this.name = name;
+        this.amountOfGoals = amountOfGoals;
+    }
+
+    public Player(String name) {
+        this(name, 0);
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int goals() {
+        return this.amountOfGoals;
+    }
+
+    public String toString() {
+        return this.name + ", goals " + this.amountOfGoals;
+    }
+
+
+}
