e1314992182de1242e62b251af1c7d358b78462b
DevJ5 <36604614+DevJ5@users.noreply.github.com>
2018-12-31 14:21:40 +0100

    Finished exercise 100

--- week6-098.CopyAndReverse/src/Main.java
+++ week6-098.CopyAndReverse/src/Main.java
@@ -1,9 +1,29 @@
 
 
 public class Main {
+    public static int[] copy(int[] array) {
+        int[] copiedArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            copiedArray[i] = array[i];
+        }
+
+        return copiedArray;
+    }
+
+    public static int[] reverseCopy(int[] array) {
+        int[] copiedArray = new int[array.length];
+
+        for (int i = 0, j = array.length -1; i < array.length; i++, j--) {
+            copiedArray[i] = array[j];
+        }
+
+        return copiedArray;
+
+    }
 
     public static void main(String[] args) {
-        // write testcode here
+
     }
-    
+
 }
--- week6-100.NightSky/src/Main.java
+++ week6-100.NightSky/src/Main.java
@@ -2,6 +2,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        // Test your code here
+        NightSky sky = new NightSky(0.1, 40, 1);
+        sky.print();
+        System.out.println(sky.starsInLastPrint());
     }
 }
